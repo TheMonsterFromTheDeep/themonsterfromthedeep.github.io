8956e874e4a94591b2426a652666350220e34e46
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-09-26 11:25:21 -0600

    Move navbar generation to main.js (which should be included in every webpage)

---
+++ source/script/main.js
@@ -0,0 +1,8 @@
+var begin = function() { }
+
+funk.listen(window,'load',function() {
+	funk.ajax('GET','/nav.html',null,function(response) {
+		funk.retrieveId('nav-wrapper').innerHTML = response.text;
+		begin();
+	});
+});
