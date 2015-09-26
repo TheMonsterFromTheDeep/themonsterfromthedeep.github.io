c02683922efdb23c55a37ec49421a37a9c89cf2e
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-09-26 13:17:21 -0600

    Make all buttons defined before any other code
    Now all the button variables are defined before they are screwed with in
    any way.

--- source/script/main.js
+++ source/script/main.js
@@ -2,9 +2,11 @@ funk.listen(window,'load',function() {
 	funk.ajax('GET','/nav.html',null,function(response) {
 		funk.retrieveId('nav-wrapper').innerHTML = response.text;
 		
-		funk.ajax('GET','/source/page/nav/menu/project.html',null,function(response) {
-			var projectButton = funk.retrieveClass('nav-button')[1];
+		var buttons = funk.retrieveClass('nav-button');
+		var homeButton = buttons[0];
+		var projectButton = buttons[1];
 		
+		funk.ajax('GET','/source/page/nav/menu/project.html',null,function(response) {
 			projectButton.listen('mouseenter',function() {
 				projectButton.innerHTML = "Project" + response.text;
 			});
@@ -17,10 +19,10 @@ funk.listen(window,'load',function() {
 		if(pageType) {
 			switch(pageType) {
 				case 'home':
-					funk.setAttribute(funk.retrieveClass('nav-button')[0],'class','nav-select');
+					funk.setAttribute(homeButton,'class','nav-select');
 					break;
 				case 'project':
-					funk.setAttribute(funk.retrieveClass('nav-button')[1],'class','nav-select');
+					funk.setAttribute(projectButton,'class','nav-select');
 					break;
 			}
 		}
