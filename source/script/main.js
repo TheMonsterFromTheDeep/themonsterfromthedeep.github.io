111a2bc7fb5d07d74f3b06100f07952a75343490
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-09-26 11:39:15 -0600

    For some reason the thing doesn't seem to be running main.js so I'm debugging it

--- source/script/main.js
+++ source/script/main.js
@@ -1,5 +1,7 @@
 var begin = function() { }
 
+alert('retrieving document...');
+
 window.onload = function() {
 	funk.ajax('GET','/nav.html',null,function(response) {
 		funk.retrieveId('nav-wrapper').innerHTML = response.text;
