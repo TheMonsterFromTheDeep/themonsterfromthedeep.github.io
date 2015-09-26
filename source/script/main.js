8571705988ccdb11bccb8098176040c331a5e450
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-09-26 12:55:49 -0600

    Update way tabs in navbar are selected

--- source/script/main.js
+++ source/script/main.js
@@ -1,8 +1,15 @@
-if(!begin) { var begin = function() { } }
-
 funk.listen(window,'load',function() {
 	funk.ajax('GET','/nav.html',null,function(response) {
 		funk.retrieveId('nav-wrapper').innerHTML = response.text;
-		begin();
+		if(pageType) {
+			switch(pageType) {
+				case 'home':
+					funk.setAttribute(funk.retrieveClass('nav-button')[0],'class','nav-select');
+					break;
+				case 'project':
+					funk.setAttribute(funk.retrieveClass('nav-button')[1],'class','nav-select');
+					break;
+			}
+		}
 	});
 });
