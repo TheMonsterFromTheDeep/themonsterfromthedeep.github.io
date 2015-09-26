3636a95ec90bcc35720b58e0e194fab55199c8bb
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-09-26 12:48:40 -0600

    Actually call begin() function

--- source/script/main.js
+++ source/script/main.js
@@ -3,5 +3,6 @@ if(!begin) { var begin = function() { } }
 funk.listen(window,'load',function() {
 	funk.ajax('GET','/nav.html',null,function(response) {
 		funk.retrieveId('nav-wrapper').innerHTML = response.text;
+		begin();
 	});
 });
